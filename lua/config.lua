b201acca47ba009beaf4aa012a7f50622ae7f745
himalaya-kumar <itshimalaykumarx@gmail.com>
2022-11-27 21:51:47 +0530

    added value again

--- lua/config.lua
+++ lua/config.lua
@@ -11,8 +11,8 @@ vim.cmd[[set list]]
 -- vim.cmd[[au VimEnter *  NvimTreeToggle]]
 vim.opt.termguicolors = true
 
---vim.cmd[[inoremap <silent><expr> <tab> pumvisible() ? coc#_select_confirm() : "\<C-g>u\<TAB>"]]
---vim.cmd[[inoremap <silent><expr> <cr> "\<c-g>u\<CR>"]]
+-- vim.cmd[[inoremap <silent><expr> <tab> pumvisible() ? coc#_select_confirm() : "\<C-g>u\<TAB>"]]
+-- vim.cmd[[inoremap <silent><expr> <cr> "\<c-g>u\<CR>"]]
 vim.cmd[[inoremap ii <Esc>]]
 
 vim.cmd[[map <C-n> :NvimTreeToggle<CR>]]
@@ -22,3 +22,6 @@ vim.cmd[[map <C-m> :BufferNext<CR>]]
 -- For FOlding code 
 -- vim.opt.foldmethod = "expr"
 -- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+--
+vim.api.nvim_exec([[inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"]],false)
+vim.api.nvim_exec([[inoremap <silent><expr> <c-space> coc#refresh()]],false)
