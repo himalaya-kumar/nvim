9e329877823fd0fb33fd2903d3e9dde8a3f61d88
himalaya-kumar <itshimalaykumarx@gmail.com>
2023-09-28 22:43:57 +0530

    migrating to lazy.nvim

---
+++ lua/themes/gruvbox_material_setup.lua
@@ -0,0 +1,14 @@
+return {
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_background = 'hard'
+    -- vim.g.gruvbox_material_background = 'soft'
+    -- vim.g.gruvbox_material_background = 'medium'
+    vim.g.gruvbox_material_better_performance = 1
+    vim.cmd([[set background=dark]])
+    -- vim.cmd([[set background=light]])
+    vim.cmd([[colorscheme gruvbox-material]])
+  end,
+}
