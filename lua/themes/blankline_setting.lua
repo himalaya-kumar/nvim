b4df5d24988003beefe8edb4999f0dfc82b16df5
himalaya-kumar <itshimalaykumarx@gmail.com>
2023-10-01 11:31:35 +0530

    migrating to lazy

--- lua/themes/blankline_setting.lua
+++ lua/themes/blankline_setting.lua
@@ -5,8 +5,78 @@ return {
 
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false,
-  }
+  config = function()
+    local status_ok, indent_blankline = pcall(require, "ibl")
+    if not status_ok then
+      return
+    end
+
+    vim.opt.list = true
+    vim.opt.listchars:append "space:⋅"
+    vim.opt.listchars:append "eol:¶"
+    vim.opt.listchars:append "tab:|⇢"
+    vim.opt.listchars:append "trail:·"
+    vim.opt.listchars:append "extends:>"
+    vim.opt.listchars:append "precedes:<"
+
+    indent_blankline.setup {
+      enable = true,
+      space_char_blankline = " ",
+      indent = {
+        char = "▎",
+        tab_char = "▎",
+        highlight = { "IndentBlanklineChar" },
+      },
+      whitespace = {
+        enable = true,
+        remove_blankline_trail = true,
+        highlight = { "IndentBlanklineSpaceChar" },
+      },
+      scope = {
+        enabled = true,
+        char = "▎",
+        show_start = true,
+        show_end = false,
+        injected_languages = true,
+        highlight = { "IndentBlanklineContextChar" },
+        priority = 1024,
+        include = {
+          node_type = {
+            ["*"] = {
+              "^argument",
+              "^expression",
+              "^for",
+              "^if",
+              "^import",
+              "^type",
+              "arguments",
+              "block",
+              "bracket",
+              "declaration",
+              "field",
+              "func_literal",
+              "function",
+              "import_spec_list",
+              "list",
+              "return_statement",
+              "short_var_declaration",
+              "statement",
+              "switch_body",
+              "try",
+              "block_mapping_pair",
+            },
+          },
+        },
+      },
+      buftype_exclude = { "terminal", "nofile", "FTerm", "alpha" },
+      exclude = {
+        "help",
+        "packer",
+        "NvimTree",
+        "conf",
+        "alpha",
+        "FTerm",
+      },
+    }
+  end
 }
