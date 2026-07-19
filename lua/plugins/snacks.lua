601acdbf39a1b7cc0a9e3fa43a1e79b8a00b704b
DONAR-0 <himalaya.kumar601@outlook.com>
2026-07-19 16:14:45 +0000

    fix: Tresitter changes

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = false },
+    indent = { enabled = true },
+    notifier = { enabled = true },
+    scroll = { enabled = true },
+    words = { enabled = true },
+  },
+}
